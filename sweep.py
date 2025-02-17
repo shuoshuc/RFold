deb558c15c987384e9c98d5e77c7aed9d83a4c0a
shawn <shuoshuc@cs.cmu.edu>
2025-02-17 14:54:18 -0500

    Refactor sweep.py to handle trace replay.

--- sweep.py
+++ sweep.py
@@ -4,64 +4,85 @@
 import multiprocessing as mp
 import os
 import subprocess
+import sys
 import time
 from itertools import product
 
-from common.flags import *
 
-# Short-hand mapping to the files.
-STRMAP = {
-    "philly": PHILLY_TRACE,
-    "ali20": ALIBABA_TRACE,
-    "acme": ACME_TRACE,
-    "helios": HELIOS_TRACE,
-    "iat": IAT_DIST,
-}
-
-
-def run_process(i, tot, args):
-    sim_dur, dim, dur_file, iat_file, policy = args
+def run_process(i, tot, cmd):
     run_dir = f"run{i}"
 
     start = time.time()
     if not os.path.exists(run_dir):
         os.makedirs(run_dir)
-    cmd = (
-        f"python3 launch.py -t {sim_dur} --dim {dim} --dur_trace_file {STRMAP[dur_file]} "
-        f"--iat_file={STRMAP[iat_file]} --sched_policy {policy} --stats_outdir {run_dir} "
-        f"--log_level WARNING"
-    )
-    result = subprocess.run(cmd, shell=True)
+    result = subprocess.run(cmd + f"--stats_outdir {run_dir}", shell=True)
     end = time.time()
 
     if result.returncode != 0:
-        print(f"[ERROR] run{i} failed with return code {result.returncode}")
+        print(f"[ERROR] run{i} failed with return code {result.returncode}", flush=True)
     print(
-        f"run {i}/{tot} took {round((end - start) / 60, 0)} min: sim_dur={sim_dur}, dim={dim}, "
-        f"dur_file={dur_file}, iat={iat_file}, sched_policy={policy}"
+        f"run {i}/{tot} took {round((end - start) / 60, 0)} min: {cmd}",
+        flush=True,
     )
 
 
-def main():
+def main(trace_folder: str):
+    # Only import flags in the function scope. common.flags imports argparse, which
+    # messes with sys.argv when loaded. Hence, delay importing until args are consumed.
+    from common.flags import (
+        PHILLY_TRACE,
+        ALIBABA_TRACE,
+        HELIOS_TRACE,
+        ACME_TRACE,
+        IAT_DIST,
+    )
+
     # All the parameters to sweep over.
     sim_duration = [50 * 3600, 100 * 3600]
     dimensions = ["16,16,16", "24,24,24", "32,32,32"]
-    duration_trace = ["philly", "ali20", "helios", "acme"]
-    iat_distribution = ["iat"]
+    if trace_folder:
+        trace = [
+            os.path.join(trace_folder, f"{trace}.csv")
+            for trace in ["philly", "alibaba20", "helios", "acme"]
+        ]
+    else:
+        trace = [PHILLY_TRACE, ALIBABA_TRACE, HELIOS_TRACE, ACME_TRACE]
+    iat = [IAT_DIST]
     sched_policy = ["firstfit", "slurm_hilbert"]
 
     start_time = time.time()
-    configs = list(
-        product(sim_duration, dimensions, duration_trace, iat_distribution, sched_policy)
-    )
+    configs = list(product(sim_duration, dimensions, trace, iat, sched_policy))
+    cmds = []
+    for args in configs:
+        sim_dur, dim, trace_file, iat_file, policy = args
+        cmd = (
+            f"python3 launch.py -t {sim_dur} --dim {dim} --sched_policy {policy} "
+            f"--log_level WARNING "
+        )
+        if trace_folder:
+            cmd += f"-r {trace_file} "
+        else:
+            cmd += f"--dur_trace_file {trace_file} --iat_file {iat_file} "
+        cmds.append(cmd)
+
     # Reserve 2 cores for the system to remain responsive.
     with mp.Pool(processes=mp.cpu_count() - 2) as pool:
         pool.starmap(
-            run_process, [(i + 1, len(configs), args) for i, args in enumerate(configs)]
+            run_process, [(i + 1, len(configs), cmd) for i, cmd in enumerate(cmds)]
         )
     end_time = time.time()
     print(f"Total execution time: {round((end_time - start_time) / 60, 0)} min")
 
 
 if __name__ == "__main__":
-    main()
+    trace_folder = None
+    if len(sys.argv) < 2:
+        print(
+            "No trace folder provided, disable trace replay. "
+            "To run replay: python sweep.py <trace_folder>"
+        )
+    else:
+        trace_folder = sys.argv[1]
+        # Drop the command line arguments to avoid argparse error.
+        sys.argv = sys.argv[:1]
+    main(trace_folder)
